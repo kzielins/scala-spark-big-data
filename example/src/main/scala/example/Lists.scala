c5fec5798be30a94d2029d1540604aa741258737
Krzysztof Zielinski <kzielins@users.noreply.github.com>
2023-02-17 16:08:48 +0100

    function update

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -22,7 +22,10 @@ object Lists:
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = xs match {
+    case Nil => 0
+    case head :: tail => head + sum(tail)
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -37,4 +40,8 @@ object Lists:
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = xs match {
+    case Nil => throw new java.util.NoSuchElementException
+    case head :: Nil => head
+    case head :: tail => Math.max(head, max(tail))
+  }
